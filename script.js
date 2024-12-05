91a2288735fff187046e54e3a8b1f58cd7a2e104
Twenty2Eleven78 <mark@e-missive.co.uk>
2024-12-05 19:26:49 +0000

    Update stopwatch format for minutes and seconds

--- script.js
+++ script.js
@@ -56,10 +56,9 @@ const Storage = {
 
 // Time formatting utility
 function formatTime(seconds) {
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
+  const minutes = Math.floor(seconds / 60);
   const secs = seconds % 60;
-  return [hours, minutes, secs]
+  return [ minutes, secs]
     .map(num => num.toString().padStart(2, '0'))
     .join(':');
 }
