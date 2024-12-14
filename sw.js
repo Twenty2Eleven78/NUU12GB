8ea1cd99cf6c62b06c45536f3a729e8f44aaf167
Twenty2Eleven78 <mark@e-missive.co.uk>
2024-12-14 22:45:02 +0000

    Update version

--- sw.js
+++ sw.js
@@ -1,21 +1,21 @@
 // Change this to your repository name
-var nugtPATH = '';
+var nugtu12gbPATH = '';
  
 // Choose a different app prefix name
-var APP_PREFIX = 'nugt_';
+var APP_PREFIX = 'nugtu12gb_';
  
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_01';
+var VERSION = 'version_131';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
-  `${nugtPATH}/`,
-  `${nugtPATH}/index.html`,
-  `${nugtPATH}/script.js`,
-  `${nugtPATH}/materialize.min.js`,
-  `${nugtPATH}/jquery-3.6.0.min.js`
+  `${nugtu12gbPATH}/`,
+  `${nugtu12gbPATH}/index.html`,
+  `${nugtu12gbPATH}/script.js`,
+  `${nugtu12gbPATH}/materialize.min.js`,
+  `${nugtu12gbPATH}/jquery-3.6.0.min.js`
 ]
