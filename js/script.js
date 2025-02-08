a86a41eafe4bcd1370186e4a50129d92e74621d0
Twenty2Eleven78 <mark@e-missive.co.uk>
2025-02-08 15:12:25 +0000

    Update Match report output

--- js/script.js
+++ js/script.js
@@ -514,7 +514,9 @@ function formatLogForWhatsApp() {
   const gameTime = formatTime(STATE.seconds);
   const team1Name = elements.Team1NameElement.textContent;
   const team2Name = elements.Team2NameElement.textContent;
-  const header = `⚽ Match Summary: ${team1Name} vs ${team2Name}\n (Game Time: ${gameTime})\n Score: ${team1Name} (${stats.teamGoals})  - ${team2Name} (${stats.oppositionGoals}) \n\n`;
+  const stats = generateStats();
+
+  const header = `⚽ Match Summary: ${team1Name} vs ${team2Name}\n (Game Time: ${gameTime})\n Score: ${team1Name} (${stats.teamGoals}) - ${team2Name} (${stats.oppositionGoals}) \n\n`;
   
   //const header = `⚽ Match Summary: ${team1Name} vs ${team2Name} (Time: ${gameTime})\n\n`;
   
@@ -535,7 +537,7 @@ function formatLogForWhatsApp() {
     })
     .join('\n');
     
-  const stats = generateStats();
+  //const stats = generateStats();
   return encodeURIComponent(`${header}${allEvents}\n\n${stats}`);
 }
 
